973df748dec586778a2d2354a60f8170b5f49299
zhenyan.guo <user@example.com>
2024-12-12 18:56:33 +0800

    fix: update vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -10,17 +9,15 @@ export default defineConfig({
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
-      dts: 'auto-imports.d.ts',
-      imports: ['vue', 'vue-router', 'pinia']
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-      dts: 'components.d.ts'
     }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1500
   }
 }) 
